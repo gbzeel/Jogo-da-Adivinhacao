107de544122ae77eb11c3be7b46713440053f3cf
Gabriel Oliveira <gbzeel@gmail.com>
2025-03-18 08:05:54 -0300

    área de impressão como variavel

--- script.js
+++ script.js
@@ -3,26 +3,27 @@ let cont = 0;
 
 function chute() {
     let palpite = document.getElementById('numSerTest').value;
+    let p = document.getElementById('resultado');
     if(cont < 10){
         if(palpite == numAleat){
-            document.getElementById('resultado').innerHTML = `Parabéns, o numero gerado pelo sistema é ${numAleat}.
+            p.innerHTML = `Parabéns, o numero gerado pelo sistema é ${numAleat}.
             Escolha outro número.`;
             cont = -1;
             numAleat = parseInt(Math.random()*100)+1;
         }else if(palpite < numAleat){
-            document.getElementById('resultado').innerHTML = `Seu palpite é menor que o número gerado. Restam ${9 - cont} palpites.`;
+            p.innerHTML = `Seu palpite é menor que o número gerado. Restam ${9 - cont} palpites.`;
         }else {
-            document.getElementById('resultado').innerHTML = `Seu palpite é maior que o número gerado. Restam ${9 - cont} palpites.`;
+            p.innerHTML = `Seu palpite é maior que o número gerado. Restam ${9 - cont} palpites.`;
         }
     }else {
-        document.getElementById('resultado').innerHTML = `Você não tem mais palpites.`
+        p.innerHTML = `Você não tem mais palpites.`
     }
     cont ++;
-    document.getElementById('numSerTest').value = "";
+    p.value = "";
 }
 
 function nova() {
     cont = 0;
     numAleat = parseInt(Math.random()*100)+1;
-    document.getElementById('resultado').innerHTML = ``;
+    p.innerHTML = ``;
 }
